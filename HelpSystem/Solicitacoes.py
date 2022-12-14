af69721e94889838248ce88481e813fccdb4d215
GustavoLoureiro960 <120188348+GustavoLoureiro960@users.noreply.github.com>
2022-12-14 14:57:29 -0300

    Progresso atual na classe de listas

--- HelpSystem/Solicitacoes.py
+++ HelpSystem/Solicitacoes.py
@@ -1,34 +1,67 @@
 class Solicitacao:
 
-    def __init__(self, tipo):
+    def __init__(self, tipo,descricao, solic):
         self.tipo = tipo
-        self.definir_tipo(tipo)
+        self.descricao = descricao
+        self.tipo_solicitacao(self.tipo)
+        self.descreva_solic(self.descricao)
 
 
-    def definir_tipo(self,tipo):
+    def registrar(self,registro):
+        registro = 0
+        self.registro = registro
+        self.registro = self.registro +1
+        return  self.registro
+
+    def tipo_solicitacao(self,tipo):
+
         self.tipo = tipo
-        print("por favor insira o tipo de sua solicitcão")
-        self.tipo = int(input("1 Solocitação para o TI,2 Solocitação para o RH,3 Solocitação de comercio,"
-                         "4 Solocitação para a area de gerenciamento, 5 Solocitação para area fiscal,"
-                         "6 Solocitação para a area de Marketing, 7 Outro tipo de Solocitação:"))
+
+        print("por favor insira a area de sua solicitcão")
+        self.tipo = int(input("Por favor digite um dos numeros a seguir referentes a sua solicitação"
+                              "1 para TI, 2 para RH, 3 area de comercio,\n4 Para a area de gerenciamento,"
+                              "5 Para a area Fiscal, 6 Para a area de Marketing,"
+                              "7 Para outro tipo de Solicitação:"))
+
         if(self.tipo == 1):
-            print("Bem vindo a area de TI, por favor nos informe a natureza de sua solicitação")
-        elif (self.tipo == 2):
-            print("Bem vindo a area de RH, por favor nos informe a natureza de sua solicitação")
-        elif (self.tipo == 3):
-            print("Bem vindo ao setor comercial, por favor nos informe a natureza de sua solicitação")
-        elif (self.tipo == 4):
-            print("Bem vindo a area de gerenciamento, por favor nos informe a natureza de sua solicitação")
-        elif (self.tipo == 5):
-            print("Bem vindo a area fiscal por favor nos informe a natureza de sua solicitação")
-        elif (self.tipo == 6):
-            print("Bem vindo a area de Marketing por favor nos informe a natureza de sua solicitação")
-        elif (self.tipo == 7):
-            print("por favor nos informe a natureza de sua solicitação")
+            print("Bem vindo a area de TI")
+
+
+        elif(self.tipo == 2):
+            print("Bem vindo a area de RH")
+
+        elif(self.tipo == 3):
+            print("Bem vindo ao setor comercial")
+
+
+        elif(self.tipo == 4):
+            print("Bem vindo a area de gerenciamento")
+
+
+        elif(self.tipo == 5):
+            print("Bem vindo a area fiscal")
+
+        elif(self.tipo == 6):
+            print("Bem vindo a area de Marketing")
+
+        elif(self.tipo == 7):
+            print("sua solicitação não se referre a uma area especifica")
+
+
         else:
-            print("Esse digito não corresponde a nenhuma Solicitação, por favor digite um numeto valido")
+            print("Esse digito não corresponde a nenhuma Solicitação, por favor digite um numero valido")
             while(self.tipo != 1 or self.tipo != 2 or self.tipo != 3 or self.tipo != 4 or self.tipo != 5
                   or self.tipo != 6 or self.tipo != 7):
-                      self.tipo = int(input("1 Solocitação para o TI,2 Solocitação para o RH,3 Solocitação de comercio,"
-                                       "4 Solocitação para a area de gerenciamento, 5 Solocitação para area fiscal,"
-                                       "6 Solocitação para a area de Marketing, 7 Outro tipo de Solocitação:"))
+                      self.tipo = int(input("Por favor digite um dos numeros a seguir referentes a sua solicitação"
+                                            "1 para TI, 2 para RH, 3 area de comercio,\n4 Para a area de gerenciamento,"
+                                            "5 Para a area Fiscal, 6 Para a area de Marketing,"
+                                            "7 Para outro tipo de Solicitação:"))
+
+        return self.tipo
+
+
+    def descreva_solic(self, descricao):
+        self.descricao = descricao
+        self.descricao = str(input("Por favor descreva a natureza de sua solicitação:"))
+
+        return self.descricao
--- HelpSystem/main.py
+++ HelpSystem/main.py
@@ -1,6 +1,6 @@
 from Contas import Conta
 from Solicitacoes import Solicitacao
 
-usuario01 = Conta("Gustavo",10000,"1000")
+#usuario01 = Conta("Gustavo",10000,"1000")
 
-pedido01 = Solicitacao(1)
+pedido01 = Solicitacao(1,"problemas com de conexção")
